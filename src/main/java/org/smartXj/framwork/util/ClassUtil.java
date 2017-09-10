8650988ced0a015478e60aa083cbacf7046c0a68
njxjxj <njxjxj@qq.com>
2017-09-10 21:26:50 +0800

    着手开发类操作工具（包含获取类加载器，加载类，加载指定包所有类）

--- src/main/java/org/smartXj/framwork/util/ClassUtil.java
+++ src/main/java/org/smartXj/framwork/util/ClassUtil.java
@@ -3,41 +3,49 @@ package org.smartXj.framwork.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.Set;
 
 /**
  * 类操作工具
  */
-public  final class ClassUtil {
-    private static  final Logger LOGGER= LoggerFactory.getLogger("ClassUtil");
+public final class ClassUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger("ClassUtil");
 
     /**
      * 获取类加载器
+     *
      * @return
      */
-    public static ClassLoader getClassLoader()
-    {
+    public static ClassLoader getClassLoader() {
         return Thread.currentThread().getContextClassLoader();
     }
 
     /**
      * 加载类
+     *
      * @param className
      * @param isInitialized
      * @return
      */
-    public static Class<?> loadClass(String className,boolean isInitialized)
-    {
-
+    public static Class<?> loadClass(String className, boolean isInitialized) {
+        Class<?> cls;
+        try {
+            cls=Class.forName(className,isInitialized,getClassLoader());
+        } catch (ClassNotFoundException e) {
+            LOGGER.error("load class failure",e);
+            throw new RuntimeException(e);
+        }
+        return cls;
     }
 
     /**
      * 获取指定包名下所有类
+     *
      * @param packageName
      * @return
      */
-    public static Set<Class<?>> getClassSet(String packageName)
-    {
+    public static Set<Class<?>> getClassSet(String packageName) {
 
     }
 
